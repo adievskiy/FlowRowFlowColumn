5cdbd55b30efd22ad68c4842891ec8bcc6edec1e
adievskiy <dievskiy@mail.ru>
2024-12-10 17:05:47 +0400

    complete

--- app/src/main/java/com/example/flowrowflowcolumn/MainActivity.kt
+++ app/src/main/java/com/example/flowrowflowcolumn/MainActivity.kt
@@ -4,26 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -86,8 +80,8 @@ fun EmployeeCard(employee: Employees) {
             .padding(8.dp)
             .width(150.dp)
             .height(100.dp)
-            .background(color = Color.DarkGray)
             .border(width = 3.dp, color = Color.Black),
+        color = Color.LightGray
     ) {
         Column(
             modifier = Modifier.padding(8.dp),
